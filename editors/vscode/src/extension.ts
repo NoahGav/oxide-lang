332b43927324e0a526953fab31e63998a20227f1
NoahGav <noahgavdev@gmail.com>
2023-11-05 14:16:45 -0600

    Got language client with analyzer running.

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -1,7 +1,26 @@
 import * as vscode from 'vscode';
 
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "oxide-analyzer" is now active!');
+
+	const serverOptions: ServerOptions = {
+		command: "oxide",
+		args: ["analyzer"],
+	};
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: "file", language: "oxide" }],
+	};
+
+	client = new LanguageClient("oxide-analyzer", serverOptions, clientOptions);
+	client.start();
 }
 
-export function deactivate() {}
+export function deactivate() {
+	if (!client) { return undefined; }
+	return client.stop();
+}
