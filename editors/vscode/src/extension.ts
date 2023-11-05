3ddd5c2794bad2d9aa21a4a00431cff5a989df82
NoahGav <noahgavdev@gmail.com>
2023-11-05 14:11:50 -0600

    Started vscode extension.

---
+++ editors/vscode/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "oxide-analyzer" is now active!');
+}
+
+export function deactivate() {}
